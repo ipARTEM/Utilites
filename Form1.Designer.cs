0828ca332b583223bdf0c25a5e369c1fcac21691
Artem <a.khimin@yandex.ru>
2021-08-03 23:40:57 +0300

    Сделал мини программки

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,7 +1,7 @@
 ﻿
 namespace Utilites
 {
-    partial class Form1
+    partial class Utilite
     {
         /// <summary>
         /// Обязательная переменная конструктора.
@@ -29,13 +29,274 @@ namespace Utilites
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.TabControl Counter;
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.файлToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.помощьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.TSMIAbout = new System.Windows.Forms.ToolStripMenuItem();
+            this.TSMIExit = new System.Windows.Forms.ToolStripMenuItem();
+            this.Счётчик1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.buttonPlus = new System.Windows.Forms.Button();
+            this.buttonMinus = new System.Windows.Forms.Button();
+            this.buttonReset = new System.Windows.Forms.Button();
+            this.labelCounter = new System.Windows.Forms.Label();
+            this.buttonRandom = new System.Windows.Forms.Button();
+            this.labelRandom = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            Counter = new System.Windows.Forms.TabControl();
+            this.menuStrip1.SuspendLayout();
+            Counter.SuspendLayout();
+            this.Счётчик1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.файлToolStripMenuItem,
+            this.помощьToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // файлToolStripMenuItem
+            // 
+            this.файлToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.TSMIExit});
+            this.файлToolStripMenuItem.Name = "файлToolStripMenuItem";
+            this.файлToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
+            this.файлToolStripMenuItem.Text = "Файл";
+            // 
+            // помощьToolStripMenuItem
+            // 
+            this.помощьToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.TSMIAbout});
+            this.помощьToolStripMenuItem.Name = "помощьToolStripMenuItem";
+            this.помощьToolStripMenuItem.Size = new System.Drawing.Size(68, 20);
+            this.помощьToolStripMenuItem.Text = "Помощь";
+            // 
+            // TSMIAbout
+            // 
+            this.TSMIAbout.Name = "TSMIAbout";
+            this.TSMIAbout.Size = new System.Drawing.Size(180, 22);
+            this.TSMIAbout.Text = "О программе";
+            this.TSMIAbout.Click += new System.EventHandler(this.TSMIAbout_Click);
+            // 
+            // TSMIExit
+            // 
+            this.TSMIExit.Name = "TSMIExit";
+            this.TSMIExit.Size = new System.Drawing.Size(180, 22);
+            this.TSMIExit.Text = "Выход";
+            this.TSMIExit.Click += new System.EventHandler(this.TSMIExit_Click);
+            // 
+            // Counter
+            // 
+            Counter.Controls.Add(this.Счётчик1);
+            Counter.Controls.Add(this.tabPage2);
+            Counter.Dock = System.Windows.Forms.DockStyle.Fill;
+            Counter.Location = new System.Drawing.Point(0, 24);
+            Counter.Name = "Counter";
+            Counter.SelectedIndex = 0;
+            Counter.Size = new System.Drawing.Size(800, 426);
+            Counter.TabIndex = 1;
+            // 
+            // Счётчик1
+            // 
+            this.Счётчик1.Controls.Add(this.labelCounter);
+            this.Счётчик1.Controls.Add(this.buttonReset);
+            this.Счётчик1.Controls.Add(this.buttonMinus);
+            this.Счётчик1.Controls.Add(this.buttonPlus);
+            this.Счётчик1.Location = new System.Drawing.Point(4, 22);
+            this.Счётчик1.Name = "Счётчик1";
+            this.Счётчик1.Padding = new System.Windows.Forms.Padding(3);
+            this.Счётчик1.Size = new System.Drawing.Size(792, 400);
+            this.Счётчик1.TabIndex = 0;
+            this.Счётчик1.Text = "Счётчик";
+            this.Счётчик1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.label3);
+            this.tabPage2.Controls.Add(this.label2);
+            this.tabPage2.Controls.Add(this.numericUpDown2);
+            this.tabPage2.Controls.Add(this.numericUpDown1);
+            this.tabPage2.Controls.Add(this.labelRandom);
+            this.tabPage2.Controls.Add(this.buttonRandom);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(792, 400);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Генератор";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // buttonPlus
+            // 
+            this.buttonPlus.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonPlus.Location = new System.Drawing.Point(245, 48);
+            this.buttonPlus.Name = "buttonPlus";
+            this.buttonPlus.Size = new System.Drawing.Size(90, 34);
+            this.buttonPlus.TabIndex = 0;
+            this.buttonPlus.Text = "+";
+            this.buttonPlus.UseVisualStyleBackColor = true;
+            this.buttonPlus.Click += new System.EventHandler(this.buttonPlus_Click);
+            // 
+            // buttonMinus
+            // 
+            this.buttonMinus.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonMinus.Location = new System.Drawing.Point(245, 209);
+            this.buttonMinus.Name = "buttonMinus";
+            this.buttonMinus.Size = new System.Drawing.Size(90, 34);
+            this.buttonMinus.TabIndex = 1;
+            this.buttonMinus.Text = "-";
+            this.buttonMinus.UseVisualStyleBackColor = true;
+            this.buttonMinus.Click += new System.EventHandler(this.buttonMinus_Click);
+            // 
+            // buttonReset
+            // 
+            this.buttonReset.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonReset.Location = new System.Drawing.Point(370, 135);
+            this.buttonReset.Name = "buttonReset";
+            this.buttonReset.Size = new System.Drawing.Size(90, 34);
+            this.buttonReset.TabIndex = 2;
+            this.buttonReset.Text = "Сброс";
+            this.buttonReset.UseVisualStyleBackColor = true;
+            this.buttonReset.Click += new System.EventHandler(this.buttonReset_Click);
+            // 
+            // labelCounter
+            // 
+            this.labelCounter.AutoSize = true;
+            this.labelCounter.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelCounter.Location = new System.Drawing.Point(277, 135);
+            this.labelCounter.Name = "labelCounter";
+            this.labelCounter.Size = new System.Drawing.Size(25, 25);
+            this.labelCounter.TabIndex = 3;
+            this.labelCounter.Text = "0";
+            // 
+            // buttonRandom
+            // 
+            this.buttonRandom.Location = new System.Drawing.Point(381, 70);
+            this.buttonRandom.Name = "buttonRandom";
+            this.buttonRandom.Size = new System.Drawing.Size(75, 23);
+            this.buttonRandom.TabIndex = 0;
+            this.buttonRandom.Text = "Задать";
+            this.buttonRandom.UseVisualStyleBackColor = true;
+            this.buttonRandom.Click += new System.EventHandler(this.buttonRandom_Click);
+            // 
+            // labelRandom
+            // 
+            this.labelRandom.AutoSize = true;
+            this.labelRandom.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelRandom.Location = new System.Drawing.Point(395, 129);
+            this.labelRandom.Name = "labelRandom";
+            this.labelRandom.Size = new System.Drawing.Size(0, 39);
+            this.labelRandom.TabIndex = 1;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(228, 70);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            100000,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown1.TabIndex = 2;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(228, 127);
+            this.numericUpDown2.Maximum = new decimal(new int[] {
+            100000,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown2.TabIndex = 3;
+            this.numericUpDown2.Value = new decimal(new int[] {
+            100000,
+            0,
+            0,
+            0});
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(161, 70);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(20, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "От";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(161, 129);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(22, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "До";
+            // 
+            // Utilite
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(Counter);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Utilite";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            Counter.ResumeLayout(false);
+            this.Счётчик1.ResumeLayout(false);
+            this.Счётчик1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem файлToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem TSMIExit;
+        private System.Windows.Forms.ToolStripMenuItem помощьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem TSMIAbout;
+        private System.Windows.Forms.TabPage Счётчик1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Button buttonReset;
+        private System.Windows.Forms.Button buttonMinus;
+        private System.Windows.Forms.Button buttonPlus;
+        public System.Windows.Forms.Label labelCounter;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label labelRandom;
+        private System.Windows.Forms.Button buttonRandom;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -10,11 +10,67 @@ using System.Windows.Forms;
 
 namespace Utilites
 {
-    public partial class Form1 : Form
+    public partial class Utilite : Form
     {
-        public Form1()
+        int count = 0;
+
+        Random random;
+
+        public Utilite()
         {
             InitializeComponent();
+            random = new Random();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TSMIExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+
+        }
+
+        private void TSMIAbout_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Программа мои утилиты,\n содержит ряд небольших программ,\n которые могут пригодиться.", "О программе");
+
+        }
+
+        private void buttonPlus_Click(object sender, EventArgs e)
+        {
+            count++;
+            labelCounter.Text = count.ToString();
+        }
+
+        private void buttonMinus_Click(object sender, EventArgs e)
+        {
+            count--;
+            labelCounter.Text = count.ToString();
+
+        }
+
+        private void buttonReset_Click(object sender, EventArgs e)
+        {
+
+            count=0;
+            labelCounter.Text = count.ToString();
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonRandom_Click(object sender, EventArgs e)
+        {
+            int num;
+            num = random.Next(Convert.ToInt32(numericUpDown1.Value), Convert.ToInt32(numericUpDown2.Value));
+
+            labelRandom.Text=num.ToString();
         }
     }
 }
